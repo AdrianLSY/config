226f05cd80e34af4cefaeeb46cb72c47086135a1
Adrian Low <adrianlow1998@gmail.com>
2025-07-09 05:58:05 +0800

    initial commit

---
+++ fish/conf.d/aliases.fish
@@ -0,0 +1,5 @@
+if status is-interactive
+    alias ff='fastfetch'
+    alias bios='sudo systemctl reboot --firmware-setup'
+    alias w11='sudo bootctl set-oneshot auto-windows && sudo reboot'
+end
---
+++ fish/config.fish
@@ -0,0 +1,6 @@
+source /usr/share/cachyos-fish-config/cachyos-config.fish
+set -x BROWSER zen-browser
+function fish_greeting
+    # Disable fastfetch
+    # fastfetch
+end
