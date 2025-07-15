38173bfb8b3f99b8f0e9f7a675690a3451d1f4a3
Adrian Low <adrianlow1998@gmail.com>
2025-07-16 01:42:30 +0800

    Update aliases.fish

--- fish/conf.d/aliases.fish
+++ fish/conf.d/aliases.fish
@@ -2,5 +2,4 @@ if status is-interactive
     alias ff='fastfetch'
     alias bios='sudo systemctl reboot --firmware-setup'
     alias uefi='sudo systemctl reboot --firmware-setup'
-    alias w11='sudo bootctl set-oneshot auto-windows && sudo reboot'
 end
