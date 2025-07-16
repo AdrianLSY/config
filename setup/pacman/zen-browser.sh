eff579e469a1eab481660827c9a03f9a9f1e8bed
Adrian Low <adrianlow1998@gmail.com>
2025-07-16 14:52:23 +0800

    set zen browser as default browser

--- setup/pacman/zen-browser.sh
+++ setup/pacman/zen-browser.sh
@@ -2,3 +2,8 @@
 set -e
 
 sudo pacman -S --noconfirm zen-browser
+
+xdg-settings set default-web-browser zen-browser.desktop
+
+# xdg-mime default zen-browser.desktop x-scheme-handler/http
+# xdg-mime default zen-browser.desktop x-scheme-handler/https
