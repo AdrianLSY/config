ab4b6f3c2f67f6ee0096125c30bc5fdd82e9aa29
Adrian Low <adrianlow1998@gmail.com>
2025-07-16 17:19:29 +0800

    updated config

---
+++ setup/flatpak/com.rawtherapee.RawTherapee.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+set -e
+
+flatpak install flathub com.rawtherapee.RawTherapee
--- setup/pacman/zen-browser.sh
+++ setup/pacman/zen-browser.sh
@@ -3,7 +3,5 @@ set -e
 
 sudo pacman -S --noconfirm zen-browser
 
-xdg-settings set default-web-browser zen-browser.desktop
-
-# xdg-mime default zen-browser.desktop x-scheme-handler/http
-# xdg-mime default zen-browser.desktop x-scheme-handler/https
+xdg-mime default zen-browser.desktop x-scheme-handler/http
+xdg-mime default zen-browser.desktop x-scheme-handler/https
