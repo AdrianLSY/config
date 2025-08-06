30360dc7693a79a5017e533935508249a53b8449
Adrian Low <adrianlow1998@gmail.com>
2025-08-07 01:23:13 +0800

    Update hidraw.sh

--- setup/tweak/hidraw.sh
+++ setup/tweak/hidraw.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
 set -e
 
-sudo chmod a+rw /dev/hidraw*
+# Allow all hidraw devices
+if [ ! -f "/etc/systemd/system/hidraw.service" ]; then
+    sudo tee /etc/systemd/system/hidraw.service > /dev/null << EOF
+[Unit]
+Description=Disable ACPI hidraw devices after boot
+After=multi-user.target
+
+[Service]
+Type=oneshot
+ExecStart=$HOME/.config/hypr/scripts/hidraw
+
+[Install]
+WantedBy=multi-user.target
+EOF
+    sudo systemctl enable hidraw.service
+fi
+
+"$HOME/.config/hypr/scripts/hidraw"
