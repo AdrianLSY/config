44da54db737e12973f3bfdb3c48bb72a87cf6822
Adrian Low <adrianlow1998@gmail.com>
2025-08-18 22:00:59 +0800

    Update zoxide.fish

--- fish/conf.d/zoxide.fish
+++ fish/conf.d/zoxide.fish
@@ -1,12 +1,16 @@
-if type -q zoxide
-    zoxide init fish | source
+set -l __zox "$HOME/.local/bin/zoxide"
+if test -x $__zox
+    $__zox init fish | source
+
     function cd
         if test (count $argv) -eq 0
             builtin cd
         else if test -d $argv[1]
             builtin cd $argv
-        else
+        else if functions -q z
             z $argv
+        else
+            builtin cd $argv
         end
     end
 end
