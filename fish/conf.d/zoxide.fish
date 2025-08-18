c74f40f371cea5926f94012f02412f4fb27d1805
Adrian Low <adrianlow1998@gmail.com>
2025-08-18 21:47:47 +0800

    Update zoxide.fish

--- fish/conf.d/zoxide.fish
+++ fish/conf.d/zoxide.fish
@@ -1,14 +1,12 @@
-zoxide init fish | source
-function cd
-    # Replaces cd with zoxide if argument is not a directory
-    if test (count $argv) -eq 0
-        # No arguments - go to home directory
-        builtin cd
-    else if test -d $argv[1]
-        # If it's a valid directory path, use regular cd
-        builtin cd $argv
-    else
-        # Otherwise, try zoxide
-        z $argv
+if type -q zoxide
+    zoxide init fish | source
+    function cd
+        if test (count $argv) -eq 0
+            builtin cd
+        else if test -d $argv[1]
+            builtin cd $argv
+        else
+            z $argv
+        end
     end
 end
