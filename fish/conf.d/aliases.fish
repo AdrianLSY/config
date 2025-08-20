6a019bafb4f2dc9a9f70e48d7dbdce2ceab3592c
Adrian Low <adrianlow1998@gmail.com>
2025-08-21 00:14:04 +0800

    updated config

--- fish/conf.d/aliases.fish
+++ fish/conf.d/aliases.fish
@@ -1,4 +1,3 @@
 alias ff='fastfetch'
-# alias sync="$HOME/.config/hypr/scripts/sync"
 alias bios='sudo systemctl reboot --firmware-setup'
 alias uefi='sudo systemctl reboot --firmware-setup'
--- fish/config.fish
+++ fish/config.fish
@@ -1,15 +1,24 @@
 source /usr/share/cachyos-fish-config/cachyos-config.fish
 set -x BROWSER zen-browser
-if tmux has-session -t 1 2>/dev/null
-    if test -n "$TMUX"
-        tmux switch-client -t 1
+
+function run_tmux
+    if tmux has-session -t 1 2>/dev/null
+        if test -n "$TMUX"
+            tmux switch-client -t 1
+        else
+            tmux attach -t 1
+        end
     else
-        tmux attach -t 1
+        tmux new -s 1 -f ~/.config/tmux/tmux.conf
     end
-else
-    tmux new -s 1 -f ~/.config/tmux/tmux.conf
 end
+
 function fish_greeting
     # Disable fastfetch
     # fastfetch
+end
+
+# Only run tmux in a pure TTY (no X11/Wayland session)
+if test -z "$DISPLAY" -a -z "$WAYLAND_DISPLAY"
+    run_tmux
 end
