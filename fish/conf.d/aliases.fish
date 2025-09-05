9202ed980395459cdb1a32dcf1f19b29ee9fdd00
Adrian Low <adrianlow1998@gmail.com>
2025-09-05 19:38:35 +0800

    added alias to reapply hyprpaper wallpaper

--- fish/conf.d/aliases.fish
+++ fish/conf.d/aliases.fish
@@ -1,3 +1,10 @@
 alias ff='fastfetch'
 alias bios='sudo systemctl reboot --firmware-setup'
 alias uefi='sudo systemctl reboot --firmware-setup'
+
+# Function to restart hyprpaper detached
+functions -e wp
+function wp --description 'Restart hyprpaper detached'
+    pkill hyprpaper 2>/dev/null
+    nohup hyprpaper -c ~/.config/hypr/hypaper.conf >/dev/null 2>&1 &
+end
