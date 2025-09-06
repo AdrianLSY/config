53f46f22d0875de9bbc1ac0b833dc5e4c046cd6d
Adrian Low <adrianlow1998@gmail.com>
2025-09-07 00:23:28 +0800

    updated config

--- fish/config.fish
+++ fish/config.fish
@@ -1,21 +1,29 @@
 source /usr/share/cachyos-fish-config/cachyos-config.fish
 set -x BROWSER zen-browser
 
-function run_tmux
-    if tmux has-session -t 1 2>/dev/null
-        if test -n "$TMUX"
-            tmux switch-client -t 1
-        else
-            tmux attach -t 1
-        end
-    else
-        tmux new -s 1 -f ~/.config/tmux/tmux.conf
-    end
-end
-
 function fish_greeting
     # Disable fastfetch
     # fastfetch
+end
+
+alias ff='fastfetch'
+alias bios='sudo systemctl reboot --firmware-setup'
+alias uefi='sudo systemctl reboot --firmware-setup'
+
+functions -e update 2>/dev/null
+function update --description 'Unattended update pacman, yay, flatpak; then cleanup'
+    sudo pacman -Syyu --noconfirm
+    yay -Syyu --noconfirm --answerclean All --answerdiff None --answeredit None
+    flatpak update -y
+    sudo pacman -Sc --noconfirm
+    yay -Sc --noconfirm
+    flatpak uninstall --unused -y
+end
+
+functions -e wp 2>/dev/null
+function wp --description 'Restart hyprpaper detached'
+    pkill hyprpaper 2>/dev/null
+    nohup hyprpaper -c ~/.config/hypr/hypaper.conf >/dev/null 2>&1 &
 end
 
 function run_tmux
