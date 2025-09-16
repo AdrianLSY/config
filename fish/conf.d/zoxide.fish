7ab21bcefe640d4501f2be24b263eb61b5309028
Adrian Low <adrianlow1998@gmail.com>
2025-09-16 22:03:06 +0800

    updated fish config

--- fish/conf.d/zoxide.fish
+++ fish/conf.d/zoxide.fish
@@ -1,16 +1,14 @@
-set -l __zox "$HOME/.local/bin/zoxide"
-if test -x $__zox
-    $__zox init fish | source
-
-    function cd
-        if test (count $argv) -eq 0
-            builtin cd
-        else if test -d $argv[1]
-            builtin cd $argv
-        else if functions -q z
-            z $argv
-        else
-            builtin cd $argv
-        end
+zoxide init fish | source
+function cd
+    # Replaces cd with zoxide if argument is not a directory
+    if test (count $argv) -eq 0
+        # No arguments - go to home directory
+        builtin cd
+    else if test -d $argv[1]
+        # If it's a valid directory path, use regular cd
+        builtin cd $argv
+    else
+        # Otherwise, try zoxide
+        z $argv
     end
 end
