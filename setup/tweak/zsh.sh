933dfa7deb17edff1fed1177099ffb70ec8c61d4
Adrian Low <adrian@rooftop.my>
2026-04-21 20:48:09 +0800

    updated config

--- setup/tweak/zsh.sh
+++ setup/tweak/zsh.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 set -e
 
+# Point zsh to ~/.config/zsh
+echo 'export ZDOTDIR="$HOME/.config/zsh"' > "$HOME/.zshenv"
+
+# Overwrite any old ~/.zshrc with a redirect to the new location
+rm -f "$HOME/.zshrc" "$HOME/.zshrc.zwc" "$HOME/.zprofile"
+
 # Add Homebrew to zsh profile
-if ! grep -q 'brew shellenv' "$HOME/.zprofile" 2>/dev/null; then
-    echo >> "$HOME/.zprofile"
-    echo 'eval "$(/opt/homebrew/bin/brew shellenv zsh)"' >> "$HOME/.zprofile"
+if ! grep -q 'brew shellenv' "$HOME/.config/zsh/.zprofile" 2>/dev/null; then
+    echo >> "$HOME/.config/zsh/.zprofile"
+    echo 'eval "$(/opt/homebrew/bin/brew shellenv zsh)"' >> "$HOME/.config/zsh/.zprofile"
 fi
 
 # Install zinit
